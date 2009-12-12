ea24262a5fb109b212fb6c3a08dd6e79efcaf722
Caio Chassot <dev@caiochassot.com>
2009-12-12 18:50:00 -0200

    Implement const setting and load path setting from .base

--- lib/basepath.rb
+++ lib/basepath.rb
@@ -2,11 +2,23 @@ require 'pathname'
 
 lambda do
   return if Object.const_defined?("BASE_PATH")
-  base        = Pathname.new(caller.last.sub(/:\d+$/, '')).dirname.realpath
-  dotbase     = '.base'
-  gotbase     = lambda { base.join(dotbase).exist? }
-  base        = base.parent until base == base.parent or gotbase[]
-  gotbase[] or raise "Can't find #{dotbase} for BASE_PATH"
-  ::BASE_PATH = base
-  $: << BASE_PATH.to_s
+
+  # find and set base
+  first_path  = (s = caller.last) ? s.sub(/:\d+$/, '') : __FILE__
+  cur_path    = Pathname.new(first_path).dirname.realpath
+  dot_base    = '.base'
+  got_base    = lambda { cur_path.join(dot_base).exist? }
+  cur_path    = cur_path.parent until cur_path == cur_path.parent or got_base[]
+  ::BASE_PATH = got_base[] ? cur_path : raise("Can't find #{dot_base} for BASE_PATH")
+
+  # read dot_base
+  base_conf = IO.read(::BASE_PATH.join(dot_base)).strip.gsub(/[ \t]/, '').gsub(/\n+/, "\n")\
+    .scan(/^\[(\w+)\]((?:\n[^\[].*)*)/)\
+    .inject({}) { |h, (k, s)| h[k.to_sym] = s.strip; h }
+
+  # set path consts
+  base_conf[:consts].scan(/([A-Z][A-Z0-9_]*)=(.+)/).each { |k, v| Object.const_set(k, ::BASE_PATH.join(v)) }
+
+  # set load_paths
+  $:.concat base_conf[:load_paths].split("\n").map { |s| ::BASE_PATH.join(s) }
 end.call
