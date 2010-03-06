39afe3b09e0e408aeda89cddb57b6b094d28298e
Caio Chassot <dev@caiochassot.com>
2010-03-06 10:07:38 -0300

    alignment

--- lib/basepath.rb
+++ lib/basepath.rb
@@ -17,14 +17,13 @@ end
 
 lambda do
   return if Object.const_defined?("BASE_PATH")
-
   # find and set base
-  first_path  = (s = caller.last) ? s.sub(/:\d+(?::in `.*?')?$/, '') : __FILE__ # file is just for when basepath is called directly
-  cur_path    = Pathname.new(first_path).dirname.realpath
-  dot_base    = '.base'
-  got_base    = lambda { cur_path.join(dot_base).exist? }
-  cur_path    = cur_path.parent until cur_path == cur_path.parent or got_base[]
-  ::BASE_PATH = got_base[] ? cur_path : raise("Can't find #{dot_base} for BASE_PATH. (started at #{first_path})")
+  first_path    = (s = caller.last) ? s.sub(/:\d+(?::in `.*?')?$/, '') : __FILE__ # file is just for when basepath is called directly
+  cur_path      = Pathname.new(first_path).dirname.realpath
+  dot_base      = '.base'
+  got_base      = lambda { cur_path.join(dot_base).exist? }
+  cur_path      = cur_path.parent until cur_path == cur_path.parent or got_base[]
+  ::BASE_PATH   = got_base[] ? cur_path : raise("Can't find #{dot_base} for BASE_PATH. (started at #{first_path})")
 
   # read dot_base
   base_conf = IO.read(::BASE_PATH.join(dot_base)).strip.gsub(/[ \t]/, '').gsub(/\n+/, "\n")\
